e8cbabc72fb696e2596222a40732f34b929fc3c2
Daniel Ko <dankomong@gmail.com>
2019-03-13 20:16:59 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
 # code here!
+class School
+  attr_accessor :roster
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade].push(name)
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort
+    @roster.each do |grade, arr|
+      @roster[grade] = arr.sort {|a, b| a[0] <=> b[0]}
+    end
+  end
+
+end
